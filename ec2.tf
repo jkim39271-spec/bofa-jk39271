25e48d1b69ab0dc12c02a4556688185ac44780bf
jenbernurye21 <nuryejenber@gmail.com>
2026-01-08 17:26:47 +0000

    security group

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,9 @@ resource "aws_instance" "example" {
   ami           = var.Jae-ami
   instance_type = var.vm-size
 
+# applying security group to ec2
+vpc_security_group_ids = [ aws_security_group.allow_tls.id ]
+
   tags = {
     Name = var.vm-name
   }
